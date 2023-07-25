7e613ba2aeb249072e062b5a813965cbe20f9c05
yeonsp <2nndspring@gmail.com>
2023-07-26 00:27:00 +0900

    module

--- vue-todo/src/main.js
+++ vue-todo/src/main.js
@@ -4,7 +4,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import store from './store'
+import { store } from './store/index'
 
 library.add(fas);
 library.add(far);
